da90e907374670872a1c5901bed2d312e604c75f
Kacper Ledwosiński <kl@airotec.pl>
2022-07-29 14:43:08 +0200

    user menu

--- frontend/src/views/Layout/Layout.tsx
+++ frontend/src/views/Layout/Layout.tsx
@@ -7,8 +7,10 @@ export const Layout = () => {
         <nav className={"flex h-20 p-10 justify-center items-center border-b-2"}>
             <div className={"flex justify-start items-center w-full space-x-12"}>
                 <Link to={"/"}><p className={"text-2xl"}>findsquad.com</p></Link>
-                <Link to={"/search"}><p className={"text-md"}>Search games</p></Link>
-                <Link to={"/game/new"}><p className={"text-md"}>Create game</p></Link>
+                <span className={"md:hidden"}>
+                    <Link to={"/search"}><p className={"text-md"}>Search games</p></Link>
+                    <Link to={"/game/new"}><p className={"text-md"}>Create game</p></Link>
+                </span>
             </div>
             <UserStatus />
         </nav>
--- frontend/src/views/Layout/UserStatus.tsx
+++ frontend/src/views/Layout/UserStatus.tsx
@@ -17,17 +17,33 @@ export const UserStatus = () => {
         return <></>;
     }
 
+    const styles = {
+        container: "flex bg-gray-200 items-center rounded-full space-x-2 p-1 group",
+        text: "whitespace-nowrap pl-4 pr-3 text-md",
+        icon: "h-full bg-white rounded-full p-1 h-10 w-10 overflow-hidden"
+    };
+
+    const UserIcon = () => {
+        return <div className={styles.icon}>
+            <FaUser color={"black"} className={"h-7 w-7 m-auto"} />
+        </div>;
+    };
+
     if (!keycloak.authenticated) {
         return <a href={keycloak.createLoginUrl()}>
-            <div className={"flex border rounded-full space-x-2"}>
-                <p>login</p>
-                <div className={"p-1 border rounded-full"}><FaUser color={"black"} /></div>
+            <div className={styles.container}>
+                <p className={styles.text}>Login or sign up!</p>
+                <UserIcon />
             </div>
         </a>;
     }
 
-    return <div className={"flex border rounded-full items-center"}>
-        <p className={"my-1 mx-2 whitespace-nowrap"}>{`Hi, ${userInfo?.username}!` ?? "unknown"}</p>
-        <div className={"p-1 border rounded-full"}><FaUser color={"black"} /></div>
+    return <div className={styles.container}>
+        <p className={styles.text}>{`Hi, ${userInfo?.username}!` ?? "unknown"}</p>
+        <div className={"flex-row hidden group-hover:flex space-x-4 pr-2"}>
+            <a href={keycloak.createLogoutUrl()}><p>logout</p></a>
+            <p>profile</p>
+        </div>
+        <UserIcon />
     </div>;
 };
