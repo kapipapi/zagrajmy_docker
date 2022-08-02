04c7e3aec1930d93937ba99064c8c1f00b8240b9
Kacper Ledwosiński <kl@airotec.pl>
2022-08-02 14:00:27 +0200

    home page

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>
-}
+    return <div
+        className={"w-full h-full bg-cover bg-center bg-[url('https://www.lm.pl/media/news_foto/136450-w-kole-oddano-do-uzytku-pelowymiarowe-boisko-ze-sztuczna-nawierzchnia_1200.jpg')]"}>
+        <div
+            className={"flex flex-col w-full h-full items-center justify-center bg-gradient-to-t from-green-500/30 to-blue-400/40 text-white opacity-100"}>
+            <div className={"text-center w-full p-5 bg-black/20 shadow-2xl"}>
+                <p className={"text-4xl font-bold"}>Zagraj z nami!</p>
+            </div>
+            <div
+                className={"flex flex-col absolute bottom-0 text-green-900 bg-white/50 backdrop-blur-sm p-5 w-full justify-center"}>
+                <p>Ciekawe rozgrywki:</p>
+                <div className={"grid grid-cols-5 gap-5"}>
+                    <div className={"border p-5"}>game 1</div>
+                    <div className={"border p-5"}>game 2</div>
+                    <div className={"border p-5"}>game 3</div>
+                </div>
+            </div>
+        </div>
+    </div>;
+};
